a9e9619bee2b066b0ce31e302239f4a8b773bf8f
OrionAF <einar95@yahoo.com>
2025-12-15 16:01:46 +0000

    Remove basket snap map feature

--- src/data/catalog.ts
+++ src/data/catalog.ts
@@ -1,11 +1,5 @@
-import { BasketPlacementConfig, BasketType } from "../types/basket";
+import { BasketType } from "../types/basket";
 import { SPECS } from "../components/geometry/constants";
-
-const DEFAULT_PLACEMENT: BasketPlacementConfig = {
-  startSnaps: {
-    excludeCorners: true,
-  },
-};
 
 export const BASKETS: BasketType[] = [
   {
@@ -13,13 +7,11 @@ export const BASKETS: BasketType[] = [
     name: "600×400×200",
     dimensions: { width: 600, depth: 400, height: 200 },
     specs: SPECS,
-    placement: DEFAULT_PLACEMENT,
   },
   {
     id: "b-600-400-100",
     name: "600×400×100",
     dimensions: { width: 600, depth: 400, height: 100 },
     specs: SPECS,
-    placement: DEFAULT_PLACEMENT,
   },
 ];
